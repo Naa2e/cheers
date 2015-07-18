2fd09bfe7e7479431e350ee563211f79974a602a
Nicole Ahima <Nicole.Ahima@outlook.com>
2015-07-17 23:49:00 -0500

    add systems

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -7,15 +7,15 @@ namespace Cheers
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Whach yo name is!?");
-			string name = Console.ReadLine ().ToUpper ();
-			string anLetters = "AEFHILMNORSX";	
+			wstring name = Console.ReadLine ().ToUpper ();
+			wstring anLetters = "AEFHILMNORSX";	
 			foreach (char c in name) 
 			{
 				if (anLetters.Contains(c))
 				{
-					Console.WriteLine("Give me an ...  {0}", 1);
+					Console.WriteLine("Give me an ...  {0}");
 				}
-				else Console.WriteLine("Give me a ..... {0}", 1);
+				else Console.WriteLine("Give me a ..... {0}");
 			}
 			Console.WriteLine("{0} is ... GRAND", name);
 			Console.ReadLine();
